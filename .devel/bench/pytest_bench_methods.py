963348782346b01f9db954ba1c6dd759efcc9e33
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2026-06-08 23:45:31 +0200

    Benchmark warm-up

--- .devel/bench/pytest_bench_methods.py
+++ .devel/bench/pytest_bench_methods.py
@@ -21,6 +21,7 @@ TEST_CASES = [
 def test_lw_analytical(benchmark, p, n, func):
     _, sc, _ = get_large_sample_cov(p=p, n=n, seed=42)
     lam, _ = np.linalg.eigh(sc)
+    _ = func(lam, n)  # Warm-up
     benchmark.group = f"LW_Analytical | (p={p}, n={n})"
     benchmark(func, lam, n)
 
@@ -29,6 +30,7 @@ def test_lw_analytical(benchmark, p, n, func):
 @pytest.mark.parametrize("func", [ref_lw_linear, lw_linear])
 def test_lw_linear(benchmark, p, n, func):
     X, _, _ = get_large_sample_cov(p=p, n=n, seed=42)
+    _ = func(X, assume_centered=False)  # Warm-up
     benchmark.group = f"LW_Linear | (p={p}, n={n})"
     benchmark(func, X, assume_centered=False)
 
@@ -37,5 +39,6 @@ def test_lw_linear(benchmark, p, n, func):
 @pytest.mark.parametrize("func", [ref_oas, oas])
 def test_oas(benchmark, p, n, func):
     _, sc, _ = get_large_sample_cov(p=p, n=n, seed=42)
+    _ = func(sc, n, p)  # Warm-up
     benchmark.group = f"OAS | (p={p}, n={n})"
     benchmark(func, sc, n, p)
