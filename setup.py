0893b9a50df9b4e2d9248696608be59939cb736b
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2026-06-14 00:12:15 +0200

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,19 @@
 import os
+import sys
 
-import numpy
+import numpy as np
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
+C_SRC_DIR = "src"
+include_dirs = [np.get_include(), C_SRC_DIR]
+library_dirs = []
 
-class HighPerfBuildExt(build_ext):
-    """Inject compiler-specific optimization flags."""
-
-    def build_extensions(self):
-        compiler_type = self.compiler.compiler_type
-
-        use_avx2 = os.environ.get("USE_AVX2", "0") == "1"
-
-        if compiler_type == "msvc":
-            c_opts = ["/O2", "/openmp"]
-            if use_avx2:
-                c_opts.append("/arch:AVX2")
-                print("Notice: Compiling with AVX2 optimizations enabled.")
-            else:
-                print(
-                    "Notice: Compiling with default MSVC instructions. Set USE_AVX2=1 to force AVX2."
-                )
-            l_opts = []
-        else:
-            c_opts = ["-O3", "-march=native", "-flto", "-fopenmp"]
-            # Link math library (-lm) and apply Link-Time Optimization (-flto)
-            l_opts = ["-flto", "-lm", "-fopenmp"]
-
-        for ext in self.extensions:
-            ext.extra_compile_args = c_opts
-            ext.extra_link_args = l_opts
-
-        super().build_extensions()
+if sys.platform == "darwin":
+    mac_includes = ["/opt/homebrew/opt/libomp/include", "/usr/local/opt/libomp/include"]
+    mac_libs = ["/opt/homebrew/opt/libomp/lib", "/usr/local/opt/libomp/lib"]
+    include_dirs.extend([p for p in mac_includes if os.path.exists(p)])
+    library_dirs.extend([p for p in mac_libs if os.path.exists(p)])
 
 
 ext_modules = [
@@ -42,9 +23,45 @@ ext_modules = [
             "shrinkr/bindings.c",
             "src/c_shrinkr.c",
         ],
-        include_dirs=["src", numpy.get_include()],
+        include_dirs=include_dirs,
+        library_dirs=library_dirs,
     )
 ]
+
+
+class HighPerfBuildExt(build_ext):
+    """Inject compiler-specific optimization flags."""
+
+    def build_extensions(self):
+        compiler_type = self.compiler.compiler_type
+
+        compile_args = []
+        link_args = []
+
+        if sys.platform == "win32":
+            compile_args.extend(["/O2", "/openmp"])
+            use_avx2 = os.environ.get("USE_AVX2", "0") == "1"
+            if compiler_type == "msvc" and use_avx2:
+                compile_args.append("/arch:AVX2")
+            else:
+                print(
+                    "Notice: Compiling with default MSVC instructions. Set USE_AVX2=1 to force AVX2."
+                )
+        elif sys.platform == "darwin":
+            compile_args.extend(["-O3", "-flto", "-Xpreprocessor", "-fopenmp"])
+            link_args.extend(["-lomp", "-lm", "-flto"])
+        elif sys.platform == "linux":
+            compile_args.extend(["-O3", "-march=native", "-flto", "-fopenmp"])
+            link_args.extend(["-flto", "-lm", "-fopenmp"])
+        else:
+            print("Unsupported platform detected. Contact package developer.")
+
+        for ext in self.extensions:
+            ext.extra_compile_args = compile_args
+            ext.extra_link_args = link_args
+
+        super().build_extensions()
+
 
 setup(
     packages=["shrinkr"],
